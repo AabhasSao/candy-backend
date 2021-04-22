8271fb003db6922e404a071d5582a58d691f8f48
Aabhas <aabhassao0@gmail.com>
2021-04-22 18:10:03 +0530

    connected to local db
    Signed-off-by: Aabhas <aabhassao0@gmail.com>

---
+++ db/connect.js
@@ -0,0 +1,22 @@
+const {Sequelize} = require('sequelize')
+
+const sequelize = new Sequelize({
+    host: 'localhost',
+    database: 'candy',
+    username: 'kirito',
+    password: '123',
+    dialect: 'mysql',
+})
+
+const auth = () => {
+    try {
+        sequelize.authenticate()
+            .then(() => console.log('connected to db'))
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+module.exports = {
+    auth
+}
--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = process.env.port || 1337;
 
+const {auth} = require('./db/connect')
+
 app.get('/', (req, res) => {
     res.send('hello')
 })
+
+// db
+auth()
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
